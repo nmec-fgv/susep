c694dd169608f74ec65f1b36ac84be8d44771dd8
ricardob <rbrandao22@gmail.com>
2018-04-25 18:16:39 -0300

    0.1

--- data_transf.py
+++ data_transf.py
@@ -4,6 +4,7 @@
 
 import os
 import pickle
+import numpy as np
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
@@ -23,71 +24,71 @@ def data_transf(data):
     
     Data encoding:
     base class: ano_modelo=0, passeio nac, regiao met sp, sexo masc, contrato V.M.R., classe bonus=0, tipo franquia = 2 
-    res[item][0] -> dummy dif(ano modelo, ano apolice) = 1
-    res[item][1] -> dummy dif(ano modelo, ano apolice) = 2
-    res[item][2] -> dummy dif(ano modelo, ano apolice) = 3
-    res[item][3] -> dummy dif(ano modelo, ano apolice) = 4
-    res[item][4] -> dummy dif(ano modelo, ano apolice) = 5
-    res[item][5] -> dummy dif(ano modelo, ano apolice) in [6,10]
-    res[item][6] -> dummy dif(ano modelo, ano apolice) in [11,20]
-    res[item][7] -> dummy dif(ano modelo, ano apolice) > 20
-    res[item][8] -> dummy cod_tarif = 11
-    res[item][9] -> dummy cod_tarif = 14A
-    res[item][10] -> dummy cod_tarif = 14B
-    res[item][11] -> dummy cod_tarif = 14C
-    res[item][12] -> dummy cod_tarif = 15
-    res[item][13] -> dummy cod_tarif = 16
-    res[item][14] -> dummy cod_tarif = 17
-    res[item][15] -> dummy cod_tarif = 18
-    res[item][16] -> dummy cod_tarif = 19
-    res[item][17] -> dummy cod_tarif = 20
-    res[item][18] -> dummy cod_tarif = 21
-    res[item][19] -> dummy cod_tarif = 22
-    res[item][20] -> dummy cod_tarif = 23
-    res[item][21] -> dummy regiao = 01
-    res[item][22] -> dummy regiao = 02
-    res[item][23] -> dummy regiao = 03
-    res[item][24] -> dummy regiao = 04
-    res[item][25] -> dummy regiao = 05
-    res[item][26] -> dummy regiao = 06
-    res[item][27] -> dummy regiao = 07
-    res[item][28] -> dummy regiao = 08
-    res[item][29] -> dummy regiao = 09
-    res[item][30] -> dummy regiao = 10
-    res[item][31] -> dummy regiao = 12
-    res[item][32] -> dummy regiao = 13
-    res[item][33] -> dummy regiao = 14
-    res[item][34] -> dummy regiao = 15
-    res[item][35] -> dummy regiao = 16
-    res[item][36] -> dummy regiao = 17
-    res[item][37] -> dummy regiao = 18
-    res[item][38] -> dummy regiao = 19
-    res[item][39] -> dummy regiao = 20
-    res[item][40] -> dummy regiao = 21
-    res[item][41] -> dummy regiao = 22
-    res[item][42] -> dummy regiao = 23
-    res[item][43] -> dummy regiao = 24
-    res[item][44] -> dummy regiao = 25
-    res[item][45] -> dummy regiao = 26
-    res[item][46] -> dummy regiao = 27
-    res[item][47] -> dummy regiao = 28
-    res[item][48] -> dummy regiao = 29
-    res[item][49] -> dummy regiao = 30
-    res[item][50] -> dummy regiao = 31
-    res[item][51] -> dummy regiao = 32
-    res[item][52] -> dummy regiao = 33
-    res[item][53] -> dummy regiao = 34
-    res[item][54] -> dummy regiao = 35
-    res[item][55] -> dummy regiao = 36
-    res[item][56] -> dummy regiao = 37
-    res[item][57] -> dummy regiao = 38
-    res[item][58] -> dummy regiao = 39
-    res[item][59] -> dummy regiao = 40
-    res[item][60] -> dummy regiao = 41
-    res[item][61] -> dummy sexo = F
-    res[item][62] -> continuous idade
-    res[item][63] -> k variable - claims count
-    res[item][64] -> d variable - exposure in years
+    res[item][0] -> exposure in years
+    res[item][1] -> constant 1
+    res[item][2] -> dummy dif(ano modelo, ano apolice) = 1
+    res[item][3] -> dummy dif(ano modelo, ano apolice) = 2
+    res[item][4] -> dummy dif(ano modelo, ano apolice) = 3
+    res[item][5] -> dummy dif(ano modelo, ano apolice) = 4
+    res[item][6] -> dummy dif(ano modelo, ano apolice) = 5
+    res[item][7] -> dummy dif(ano modelo, ano apolice) in [6,10]
+    res[item][8] -> dummy dif(ano modelo, ano apolice) in [11,20]
+    res[item][9] -> dummy dif(ano modelo, ano apolice) > 20
+    res[item][10] -> dummy cod_tarif = 11
+    res[item][11] -> dummy cod_tarif = 14A
+    res[item][12] -> dummy cod_tarif = 14B
+    res[item][13] -> dummy cod_tarif = 14C
+    res[item][14] -> dummy cod_tarif = 15
+    res[item][15] -> dummy cod_tarif = 16
+    res[item][16] -> dummy cod_tarif = 17
+    res[item][17] -> dummy cod_tarif = 18
+    res[item][18] -> dummy cod_tarif = 19
+    res[item][19] -> dummy cod_tarif = 20
+    res[item][20] -> dummy cod_tarif = 21
+    res[item][21] -> dummy cod_tarif = 22
+    res[item][22] -> dummy cod_tarif = 23
+    res[item][23] -> dummy regiao = 01
+    res[item][24] -> dummy regiao = 02
+    res[item][25] -> dummy regiao = 03
+    res[item][26] -> dummy regiao = 04
+    res[item][27] -> dummy regiao = 05
+    res[item][28] -> dummy regiao = 06
+    res[item][29] -> dummy regiao = 07
+    res[item][30] -> dummy regiao = 08
+    res[item][31] -> dummy regiao = 09
+    res[item][32] -> dummy regiao = 10
+    res[item][33] -> dummy regiao = 12
+    res[item][34] -> dummy regiao = 13
+    res[item][35] -> dummy regiao = 14
+    res[item][36] -> dummy regiao = 15
+    res[item][37] -> dummy regiao = 16
+    res[item][38] -> dummy regiao = 17
+    res[item][39] -> dummy regiao = 18
+    res[item][40] -> dummy regiao = 19
+    res[item][41] -> dummy regiao = 20
+    res[item][42] -> dummy regiao = 21
+    res[item][43] -> dummy regiao = 22
+    res[item][44] -> dummy regiao = 23
+    res[item][45] -> dummy regiao = 24
+    res[item][46] -> dummy regiao = 25
+    res[item][47] -> dummy regiao = 26
+    res[item][48] -> dummy regiao = 27
+    res[item][49] -> dummy regiao = 28
+    res[item][50] -> dummy regiao = 29
+    res[item][51] -> dummy regiao = 30
+    res[item][52] -> dummy regiao = 31
+    res[item][53] -> dummy regiao = 32
+    res[item][54] -> dummy regiao = 33
+    res[item][55] -> dummy regiao = 34
+    res[item][56] -> dummy regiao = 35
+    res[item][57] -> dummy regiao = 36
+    res[item][58] -> dummy regiao = 37
+    res[item][59] -> dummy regiao = 38
+    res[item][60] -> dummy regiao = 39
+    res[item][61] -> dummy regiao = 40
+    res[item][62] -> dummy regiao = 41
+    res[item][63] -> dummy sexo = F
+    res[item][64] -> continuous idade
     res[item][65] -> dummy codigo_contrato = 2 (valor definido)
     res[item][66] -> dummy classe bonus = 1
     res[item][67] -> dummy classe bonus = 2
@@ -114,150 +115,150 @@ def data_transf(data):
     '''
 
     res = []
-    for item, x in enumerate(data):
+    for item, x in enumerate(data0):
         res.append([0] * 88)
+        res[item][1] = 1
         res[item][87] = []
         if x[1] >= 2000 + int(aa):
             pass
         elif (2000 + int(aa) - x[1]) == 1:
-            res[item][0] = 1
-        elif (2000 + int(aa) - x[1]) == 2:
-            res[item][1] = 1
-        elif (2000 + int(aa) - x[1]) == 3:
             res[item][2] = 1
-        elif (2000 + int(aa) - x[1]) == 4:
+        elif (2000 + int(aa) - x[1]) == 2:
             res[item][3] = 1
-        elif (2000 + int(aa) - x[1]) == 5:
+        elif (2000 + int(aa) - x[1]) == 3:
             res[item][4] = 1
-        elif (2000 + int(aa) - x[1]) >= 6 and (2000 + int(aa) - x[1]) <= 10:
+        elif (2000 + int(aa) - x[1]) == 4:
             res[item][5] = 1
-        elif (2000 + int(aa) - x[1]) >= 11 and (2000 + int(aa) - x[1]) <= 20:
+        elif (2000 + int(aa) - x[1]) == 5:
             res[item][6] = 1
-        elif (2000 + int(aa) - x[1]) > 20:
+        elif (2000 + int(aa) - x[1]) >= 6 and (2000 + int(aa) - x[1]) <= 10:
             res[item][7] = 1
+        elif (2000 + int(aa) - x[1]) >= 11 and (2000 + int(aa) - x[1]) <= 20:
+            res[item][8] = 1
+        elif (2000 + int(aa) - x[1]) > 20:
+            res[item][9] = 1
 
         if x[2] == ' 11':
-            res[item][8] = 1
-        if x[2] == '14A':
-            res[item][9] = 1
-        if x[2] == '14B':
             res[item][10] = 1
-        if x[2] == '14C':
+        if x[2] == '14A':
             res[item][11] = 1
-        if x[2] == ' 15':
+        if x[2] == '14B':
             res[item][12] = 1
-        if x[2] == ' 16':
+        if x[2] == '14C':
             res[item][13] = 1
-        if x[2] == ' 17':
+        if x[2] == ' 15':
             res[item][14] = 1
-        if x[2] == ' 18':
+        if x[2] == ' 16':
             res[item][15] = 1
-        if x[2] == ' 19':
+        if x[2] == ' 17':
             res[item][16] = 1
-        if x[2] == ' 20':
+        if x[2] == ' 18':
             res[item][17] = 1
-        if x[2] == ' 21':
+        if x[2] == ' 19':
             res[item][18] = 1
-        if x[2] == ' 22':
+        if x[2] == ' 20':
             res[item][19] = 1
-        if x[2] == ' 23':
+        if x[2] == ' 21':
             res[item][20] = 1
+        if x[2] == ' 22':
+            res[item][21] = 1
+        if x[2] == ' 23':
+            res[item][22] = 1
 
         if x[3] == '01':
-            res[item][21] = 1
-        if x[3] == '02':
-            res[item][22] = 1
-        if x[3] == '03':
             res[item][23] = 1
-        if x[3] == '04':
+        if x[3] == '02':
             res[item][24] = 1
-        if x[3] == '05':
+        if x[3] == '03':
             res[item][25] = 1
-        if x[3] == '06':
+        if x[3] == '04':
             res[item][26] = 1
-        if x[3] == '07':
+        if x[3] == '05':
             res[item][27] = 1
-        if x[3] == '08':
+        if x[3] == '06':
             res[item][28] = 1
-        if x[3] == '09':
+        if x[3] == '07':
             res[item][29] = 1
-        if x[3] == '10':
+        if x[3] == '08':
             res[item][30] = 1
-        if x[3] == '12':
+        if x[3] == '09':
             res[item][31] = 1
-        if x[3] == '13':
+        if x[3] == '10':
             res[item][32] = 1
-        if x[3] == '14':
+        if x[3] == '12':
             res[item][33] = 1
-        if x[3] == '15':
+        if x[3] == '13':
             res[item][34] = 1
-        if x[3] == '16':
+        if x[3] == '14':
             res[item][35] = 1
-        if x[3] == '17':
+        if x[3] == '15':
             res[item][36] = 1
-        if x[3] == '18':
+        if x[3] == '16':
             res[item][37] = 1
-        if x[3] == '19':
+        if x[3] == '17':
             res[item][38] = 1
-        if x[3] == '20':
+        if x[3] == '18':
             res[item][39] = 1
-        if x[3] == '21':
+        if x[3] == '19':
             res[item][40] = 1
-        if x[3] == '22':
+        if x[3] == '20':
             res[item][41] = 1
-        if x[3] == '23':
+        if x[3] == '21':
             res[item][42] = 1
-        if x[3] == '24':
+        if x[3] == '22':
             res[item][43] = 1
-        if x[3] == '25':
+        if x[3] == '23':
             res[item][44] = 1
-        if x[3] == '26':
+        if x[3] == '24':
             res[item][45] = 1
-        if x[3] == '27':
+        if x[3] == '25':
             res[item][46] = 1
-        if x[3] == '28':
+        if x[3] == '26':
             res[item][47] = 1
-        if x[3] == '29':
+        if x[3] == '27':
             res[item][48] = 1
-        if x[3] == '30':
+        if x[3] == '28':
             res[item][49] = 1
-        if x[3] == '31':
+        if x[3] == '29':
             res[item][50] = 1
-        if x[3] == '32':
+        if x[3] == '30':
             res[item][51] = 1
-        if x[3] == '33':
+        if x[3] == '31':
             res[item][52] = 1
-        if x[3] == '34':
+        if x[3] == '32':
             res[item][53] = 1
-        if x[3] == '35':
+        if x[3] == '33':
             res[item][54] = 1
-        if x[3] == '36':
+        if x[3] == '34':
             res[item][55] = 1
-        if x[3] == '37':
+        if x[3] == '35':
             res[item][56] = 1
-        if x[3] == '38':
+        if x[3] == '36':
             res[item][57] = 1
-        if x[3] == '39':
+        if x[3] == '37':
             res[item][58] = 1
-        if x[3] == '40':
+        if x[3] == '38':
             res[item][59] = 1
-        if x[3] == '41':
+        if x[3] == '39':
             res[item][60] = 1
+        if x[3] == '40':
+            res[item][61] = 1
+        if x[3] == '41':
+            res[item][62] = 1
 
         if x[6] == 'F':
-            res[item][61] = 1
+            res[item][63] = 1
 
-        res[item][62] += relativedelta(x[4], x[7]).years
-        res[item][62] += relativedelta(x[4], x[7]).months / 12
-        res[item][62] += relativedelta(x[4], x[7]).days / 365.2425
+        res[item][64] += relativedelta(x[4], x[7]).years
+        res[item][64] += relativedelta(x[4], x[7]).months / 12
+        res[item][64] += relativedelta(x[4], x[7]).days / 365.2425
 
         if x[8] == None and x[9] == None:
-            res[item][63] = 0
             delta_years = 0
             delta_years += relativedelta(x[5], x[4]).years
             delta_years += relativedelta(x[5], x[4]).months / 12
             delta_years += relativedelta(x[5], x[4]).days / 365.2425
-            res[item][64] = delta_years
+            res[item][0] = delta_years
         elif x[8] == None and x[9] != None:
             fim_vig = x[5]
             for i in x[9].values():
@@ -267,35 +268,30 @@ def data_transf(data):
                     else:
                         fim_vig = datetime.strptime(i['f2'], '%Y-%m-%d').date()
 
-            res[item][63] = 0
             delta_years = 0
             delta_years += relativedelta(fim_vig, x[4]).years
             delta_years += relativedelta(fim_vig, x[4]).months / 12
             delta_years += relativedelta(fim_vig, x[4]).days / 365.2425
-            res[item][64] = delta_years
+            res[item][0] = delta_years
         elif x[8] != None and x[9] == None:
-            sin_count = 0
             aux_dict = {}
             for j, k in zip(x[8].keys(), x[8].values()):
                 if j in {'1', '2', '3', '4', '5', '6'}:
                     if k['f1'] > 0:
                         if k['f2'] not in aux_dict.keys():
                             aux_dict[k['f2']] = k['f1']
-                            sin_count += 1
                         else:
                             aux_dict[k['f2']] += k['f1']
 
             for i in aux_dict.values():
                 res[item][87].append(i)
 
-            res[item][63] = sin_count
             delta_years = 0
             delta_years += relativedelta(x[5], x[4]).years
             delta_years += relativedelta(x[5], x[4]).months / 12
             delta_years += relativedelta(x[5], x[4]).days / 365.2425
-            res[item][64] = delta_years
+            res[item][0] = delta_years
         else:
-            sin_count = 0
             aux_dict = {}
             fim_vig = x[5]
             for j, k in zip(x[8].keys(), x[8].values()):
@@ -303,7 +299,6 @@ def data_transf(data):
                     if k['f1'] > 0:
                         if k['f2'] not in aux_dict.keys():
                             aux_dict[k['f2']] = k['f1']
-                            sin_count += 1
                         else:
                             aux_dict[k['f2']] += k['f1']
 
@@ -317,12 +312,11 @@ def data_transf(data):
                     else:
                         fim_vig = datetime.strptime(i['f2'], '%Y-%m-%d').date()
             
-            res[item][63] = sin_count
             delta_years = 0
             delta_years += relativedelta(fim_vig, x[4]).years
             delta_years += relativedelta(fim_vig, x[4]).months / 12
             delta_years += relativedelta(fim_vig, x[4]).days / 365.2425
-            res[item][64] = delta_years
+            res[item][0] = delta_years
 
         if x[0] == '2':
             res[item][65] = 1
@@ -368,14 +362,36 @@ def data_transf(data):
     return res
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     months = ('jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez')
     years = ('08', '09', '10', '11')
     for mmm in months:
         for aa in years:
             filename = 'data_' + mmm + aa + '_raw.pkl'
-            data = load_pkl(filename)
-            results = data_transf(data)
+            data0 = load_pkl(filename)
+            data = data_transf(data0)
+            data = [item for item in data if item[0] > 1e-2]
+            y_threshold = 80000
+            y_count_mc = np.empty([len(data)])
+            y_count_ec = np.empty([len(data)])
+            y_claim_mc = np.empty([len(data)])
+            y_claim_ec = np.empty([len(data)])
+            X = np.empty([len(data), len(data[0])-1])
+            for i, item in enumerate(data):
+                y_count_mc[i] = sum(x < y_threshold for x in item[-1])
+                y_count_ec[i] = sum(x >= y_threshold for x in item[-1])
+                if len([x for x in item[-1] if x < y_threshold]) > 0:
+                    y_claim_mc[i] = sum([x for x in item[-1] if x < y_threshold])/ len([x for x in item[-1] if x < y_threshold])
+                else:
+                    y_claim_mc[i] = 0
+                if len([x for x in item[-1] if x >= y_threshold]) > 0:
+                    y_claim_ec[i] = sum([x for x in item[-1] if x >= y_threshold])/ len([x for x in item[-1] if x >= y_threshold])
+                else:
+                    y_claim_ec[i] = 0
+                X[i] = item[:-1]
+            
+            X = np.hstack((np.log(X[:,[0]]), X[:,1:]))
+            results = dict([('y_count_mc', y_count_mc), ('y_count_ec', y_count_ec), ('y_claim_mc', y_claim_mc), ('y_claim_ec', y_claim_ec), ('X', X)])
 
             try:
                 os.remove('Data/data_' + mmm + aa + '.pkl')
